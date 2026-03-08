0dd0f409e80f9e1f0b24078cb0de8832f7fd6cfc
puspa222 <pusparegmi11b@gmail.com>
2026-03-08 15:57:37 +0545

    feat(notifier): implement Send method for DiscordNotifier to send messages

--- internal/notifier/discord.go
+++ internal/notifier/discord.go
@@ -1,5 +1,12 @@
 package notifier
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 type DiscordNotifier struct{}
 
 func NewDiscordNotifier() *DiscordNotifier {
@@ -7,6 +14,24 @@ func NewDiscordNotifier() *DiscordNotifier {
 }
 
 func (d *DiscordNotifier) Send(to, message string) error {
+	payload := map[string]string{
+		"content": message,
+	}
+	body, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("failed to marshal Discord payload: %w", err)
+	}
+	resp, err := http.Post(to, "application/json", bytes.NewBuffer(body))
+	if err != nil {
+		return fmt.Errorf("failed to send Discord message: %w", err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return fmt.Errorf("Discord API returned status: %s", resp.Status)
+	}
+	fmt.Printf("[Discord] Message sent to %s\n", to)
 	return nil
 }
 
